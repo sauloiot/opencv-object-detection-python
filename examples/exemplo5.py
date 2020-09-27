a821de8e6d4b585f023952e54e2d70ff6d5c3261
sauloiot <saulo.11@hotmail.com>
2020-09-27 17:05:37 -0300

    detecção de olhos e captura de fotos

--- examples/exemplo5.py
+++ examples/exemplo5.py
@@ -1,7 +1,9 @@
 import cv2
+import numpy as np
 #captura de imagem
 
 classificador = cv2.CascadeClassifier('../cascades/haarcascade_frontalface_default.xml')
+classificadorOlhos = cv2.CascadeClassifier('../cascades/haarcascade_eye.xml')
 camera = cv2.VideoCapture(0)
 amostra = 1
 numeroAmostras = 5
@@ -12,16 +14,25 @@ print("capturando as faces .....")
 while (True):
     conectado, imagem = camera.read()
     imagemCinza = cv2.cvtColor(imagem, cv2.COLOR_BGR2GRAY)
+    #print(np.average(imagemCinza))
     facesDetectadas = classificador.detectMultiScale(imagemCinza, scaleFactor=1.5, minSize=(150, 150))
 
     for (x, y, l, a) in facesDetectadas:
         cv2.rectangle(imagem, (x, y), (x + l, y + a), (0,0,255), 2)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            imagemFace = cv2.resize(imagemCinza[y:y + a, x:x + l], (largura, altura))
+        regiao = imagem[y:y + a, x:x + l]
+        regiaoCinzaOlho = cv2.cvtColor(regiao, cv2.COLOR_BGR2GRAY)
+        olhosDetectados = classificadorOlhos.detectMultiScale(regiaoCinzaOlho)
+        #print(olhosDetectados)
+        for (ox, oy, ol, oa) in olhosDetectados:
+            cv2.rectangle(regiao, (ox, oy), (ox + ol, oy + oa), (255, 0, 255), 2)
 
-            cv2.imwrite("fotos/pessoa." + str(id) + "." + str(amostra) + ".jpg", imagemFace)
-            print("[foto "+ str(amostra) + " capturada com Sucesso]")
-            amostra += 1
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            if np.average(imagemCinza) > 110:
+                imagemFace = cv2.resize(imagemCinza[y:y + a, x:x + l], (largura, altura))
+                cv2.imwrite("../fotos/pessoa." + str(id) + "." + str(amostra) + ".jpg", imagemCinza)
+                print("[foto "+ str(amostra) + " capturada com Sucesso]")
+                amostra += 1
 
 
     cv2.imshow("Face", imagem)
